2fc136cc627a8f2a6f98a1d225c7db91eeb9269c
DanilovDaniil <Danilovdan.007@yandex.ru>
2025-05-15 23:41:28 +0400

    Update PZ_11_1.py

--- PZ_11_8/PZ_11_1.py
+++ PZ_11_8/PZ_11_1.py
@@ -9,36 +9,55 @@
 # Сумма элементов больших 10 во второй половине:
 
 
-p = ['-99 6 12 -36 20 45 100 -15 8 17 -5 23']
 f3 = open('data_3.txt', 'w', encoding='UTF-8')
-f3.writelines(p)
+print("-99, 6, 12, -36, 20, 45, 100, -15, 8, 17, -5, 23", file=f3)
 f3.close()
 
 f4 = open('data_4.txt', 'w', encoding='UTF-8')
 f4.write('Исходные данные: ')
-f4.writelines(p)
+print("-99, 6, 12, -36, 20, 45, 100, -15, 8, 17, -5, 23", file=f4)
 f4.close()
+
+f3 = open('data_3.txt', 'r', encoding='UTF-8')
+p = 0
+for i in f3:
+    n = i.split(',')
+    p += len(n)
+f3.close()
+
+f3 = open('data_3.txt', 'r', encoding='UTF-8')
+t = f3.read()
+
+numbers = []
+for i in t.split():
+    numbers.append(i)
+
+min_num = numbers[0]
+last_index = 0
+
+for i in range(len(numbers)):
+    num = numbers[i]
+    if num <= min_num:
+        min_num = num
+        last_index = i
+f3.close()
 
 f3 = open('data_3.txt', encoding='UTF-8')
 k = f3.read()
-k = k.split()
+k = k.split(', ')  # Разделяем по запятой с пробелом
 for i in range(len(k)):
     k[i] = int(k[i])
 f3.close()
 
-count = len(p)
-min_value = min(k)
-last_min = len(k) - 1 - k[::-1].index(min_value)
-
-h = len(k) // 2
-d10 = 0
-for i in k[h:]:
-    if i > 10:
-        d10 += i
+# 4. Вычисляем сумму элементов >10 во второй половине
+half = len(k) // 2
+sum_gt10 = 0
+for i in range(half, len(k)):
+    if k[i] > 10:
+        sum_gt10 += k[i]
 
 f4 = open('data_4.txt', 'a', encoding='UTF-8')
-f4.write('\n')
-print('Количество элементов:', count, file=f4)
-print('Индекс последнего минимального элемента', last_min, file=f4)
-print('Сумма элементов больших 10 во второй половине:', d10, file=f4)
+print('Количество элементов:', p, file=f4)
+print('Индекс последнего минимального элемента', last_index, file=f4)
+print('Сумма элементов больших 10 во второй половине:', sum_gt10, file=f4)
 f4.close()

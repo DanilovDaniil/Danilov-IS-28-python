91f5ee507708992fe4c269fc31e1924347f4bfc2
Danilovdan <danilovdan.007@yandex.ru>
2025-05-16 01:46:23 +0400

    Завершил ПЗ13

---
+++ PZ_13_8/PZ_13_1.py
@@ -0,0 +1,24 @@
+# В двумерном списке элементы столбца N (N задать с клавиатуры) увеличить в два
+# раза.
+
+def double_column(matrix, column):
+    for i in matrix:
+        if column < len(i):
+            i[column] *= 2
+
+
+d = int(input("Введите количество строк: "))
+cols = int(input("Введите количество столбцов: "))
+
+matrix = []
+for i in range(d):
+    b = list(map(int, input(f"Введите {cols} элементов строки {i+1} через пробел: ").split()))
+    matrix.append(b)
+
+N = int(input("Введите номер столбца N начиная с 0: "))
+
+double_column(matrix, N)
+
+print("Результат:")
+for b in matrix:
+    print(b)

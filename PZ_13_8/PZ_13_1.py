0e659fe392e4470d232c59eff9d595fdd7d87322
DanilovDaniil <Danilovdan.007@yandex.ru>
2025-05-28 15:28:32 +0400

    Update PZ_13_1.py

--- PZ_13_8/PZ_13_1.py
+++ PZ_13_8/PZ_13_1.py
@@ -2,25 +2,19 @@
 # раза.
 
 
-def double_column(matrix, column):
-    for i in matrix:
-        if column < len(i):
-            i[column] *= 2
+r = int(input("Введите количество строк: "))
+c = int(input("Введите количество столбцов: "))
 
-
-d = int(input("Введите количество строк: "))
-cols = int(input("Введите количество столбцов: "))
-
-matrix = []
-for i in range(d):
-    b = list(map(int, input(f"Введите {cols} элементов строки {i+1}: ")))
-    matrix.append(b)
+matrix = list(map(lambda i: list(map(int, input(f"Введите {c} элементов строки {i+1} через пробел: ").split())),
+                  range(r)))
 
 N = int(input("Введите номер столбца N начиная с 0: "))
 
-double_column(matrix, N)
+ind = list(range(c))
+
+result = list(map(lambda r: list(map(lambda col_index: r[col_index] * 2 if col_index == N else r[col_index],ind)),
+                  matrix))
 
 print("Результат:")
-for b in matrix:
-    print(b)
-
+for row in result:
+    print(row)

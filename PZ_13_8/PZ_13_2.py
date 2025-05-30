1d66dedc7d3db4b7622eeb69797fa86118fce123
DanilovDaniil <Danilovdan.007@yandex.ru>
2025-05-30 17:37:13 +0400

    Update PZ_13_2.py

--- PZ_13_8/PZ_13_2.py
+++ PZ_13_8/PZ_13_2.py
@@ -5,9 +5,9 @@ c = int(input("Введите количество столбцов: "))
 
 matrix = [list(map(int, input(f"Введите {c} элементов строки {i+1} через пробел: ").split())) for i in range(r)]
 
-new_matrix = list(map(lambda i: [0] * c if i == r - 1 else matrix[i], range(r)))
+result = list(map(lambda i: [0] * c if i == r - 1 else matrix[i], range(r)))
 
 print("Результат:")
-for row in new_matrix:
+for row in result:
     print(row)
 
